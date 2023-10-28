12a9b73fcd18874e1e0d5f5e660965da8a33a1be
Muhammad Shafiq Ahmad Razman <shafiq@Muhammads-MacBook-Air.local>
2023-10-28 16:26:57 +0800

    latest

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from functools import wraps
 
 app = Flask(__name__)
 
@@ -8,6 +9,21 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost:3306/pr
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+
+SECRET_TOKEN = "1234"  # You should use a more secure method to store this
+
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = None
+        if 'Authorization' in request.headers:
+            token = request.headers['Authorization'].replace("Bearer ", "")
+
+        if not token or token != SECRET_TOKEN:
+            return jsonify({'message': 'Token is missing or invalid'}), 401
+
+        return f(*args, **kwargs)
+    return decorated
 
 # Define the table model
 class Product(db.Model):
@@ -21,11 +37,13 @@ class Product(db.Model):
         }
 
 @app.route('/products', methods=['GET'])
+@token_required
 def get_products():
     products = Product.query.all()
     return jsonify([product.to_dict() for product in products])
 
 @app.route('/product', methods=['POST'])
+@token_required
 def add_product():
     product_name = request.json.get('name')
     new_product = Product(name=product_name)
